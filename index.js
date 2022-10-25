e5c4f279f616e7258cc87818cf7b788eada5ff6b
Sheikh sadi <you@example.com>
2022-10-25 18:55:00 +0600

    diynamically added

--- index.js
+++ index.js
@@ -11,11 +11,30 @@ app.get('/', (req,res) =>{
      res.send('News API Running');
 });
 
-app.get('/categories', (req, res) =>{
+app.get('/news-categories', (req, res) =>{
      res.send(categories);
 })
+
+app.get('/category/:id', (req, res) =>{
+     const id = req.params.id;
+     if(id === '08'){
+          res.send(news);
+     }
+     else{
+          const categoryNews = news.filter(n => n.category_id === id);
+          res.send(categoryNews);
+     }
+     
+})
+
 app.get('/news', (req, res) =>{
      res.send(news);
+})
+
+app.get('/news/:id', (req, res) =>{
+     const id = req.params.id;
+     const selectedNews = news.find(n => n._id === id);
+     res.send(selectedNews);
 })
 
 app.listen(port, () =>{
